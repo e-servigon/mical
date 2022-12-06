b60e46ae57880a6dd1753db5154cfce5b1b1fe05
eservigon <eservigon@tiendapago.com>
2022-12-06 12:48:16 -0600

    commit inicial v2

--- rentmodule/models.py
+++ rentmodule/models.py
@@ -4,4 +4,11 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-
+class Car(models.Model):
+    car_brand = models.CharField(max_length=100)
+    car_model = models.CharField(max_length=100)
+    car_color = models.CharField(max_length=100)
+    created_date = models.DateTimeField(default = timezone.now, blank=True, null=True)
+  
+    def __str__ (self):
+        return 'auto : %s %s %s' % (self.car_brand, self.car_model, self.car_color)
